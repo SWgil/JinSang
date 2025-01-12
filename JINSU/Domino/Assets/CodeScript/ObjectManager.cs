4351d28ece6bc3bddb7cbe1dfec8f3133eb6bd56
jinsu421 <jinsu0286@daum.net>
2025-01-12 14:33:48 +0900

    feat. 도미노 클릭생성,오브젝트매니저,크리에이터,게임매니저 코드 추가
    1.씬에 등록하는 objectCreator 스크립트를 통해서
    마우스를 클릭하는 경우: 충돌감지를 통해 해당위치에 도미노생성
    2.object생성 및 관리하는 objectmanager 스크립트 수정
    2.게임매니저 코드에서 게임진행관련(시간기능,유저정보저장) 기능 추가(적용여부는 미확인,UI와 붙일때 확인가능할듯)

--- JINSU/Domino/Assets/CodeScript/ObjectManager.cs
+++ JINSU/Domino/Assets/CodeScript/ObjectManager.cs
@@ -1,9 +1,145 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
+    private static ObjectManager instance;
+    public List<GameObject> prefabs = new List<GameObject>();    //객체들의 원본프리팹
+    private Dictionary<string, GameObject> objectPool = 
+    new Dictionary<string, GameObject>(); //실제 인스턴스들
+
+
+    public static ObjectManager Call
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindFirstObjectByType<ObjectManager>();
+                if (instance == null)
+                {
+                    GameObject container = new GameObject("ObjectManager");
+                    instance = container.AddComponent<ObjectManager>();
+                }
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    public void MemoryDelete()
+    {
+        if(instance == null)
+            return; 
+        foreach (KeyValuePair<string, GameObject> obj in objectPool)
+        {
+            Destroy(obj.Value);
+        }
+        objectPool.Clear();
+        objectPool=null;
+    }
+    void OnDestroy()
+    {
+        MemoryDelete();
+        instance = null;
+    }
+
+    public void RegisterObject(GameObject obj, string name)
+    {
+        if (!objectPool.ContainsKey(name))
+        {
+            GameObject clone = Instantiate(obj) as GameObject;
+            clone.transform.name = name;
+            clone.transform.localPosition = Vector3.zero;
+            clone.SetActive(false);
+            clone.transform.parent = transform;
+            objectPool.Add(name, obj);
+        }
+    }
+
+    //UnUsed Code
+    public GameObject MoveObject(string objName, Vector3 pos, Quaternion rot)
+    {
+        if (objectPool.ContainsKey(objName))
+        {
+
+            if (pos == null)
+            {
+                pos = Vector3.zero;
+            }
+            else
+            {
+                objectPool[objName].transform.position = pos;
+            }
+
+            if (rot == null)
+            {
+                rot = Quaternion.identity;
+            }
+            else
+            {
+                objectPool[objName].transform.rotation = rot;
+            }
+        }
+        return objectPool[objName];
+    }
+    public GameObject GetObject(string name)
+    {
+        if (objectPool.ContainsKey(name))
+        {
+            return objectPool[name];
+        }
+        return null;
+    }
+    public GameObject GetPrefabs(string name)
+{
+    if (prefabs.Count > 0)
+    {
+        foreach (GameObject obj in prefabs)
+        {
+            if(obj==null)
+                continue;
+            else if (obj.name == name)
+            {
+                return obj;
+            }
+        }
+    }
+    return null;
+}
+    public void UnregisterObject(string name)
+    {
+        if (objectPool.ContainsKey(name))
+        {
+            objectPool.Remove(name);
+        }
+    }
+
+    public Dictionary<string, GameObject> GetObjectPool()
+    {
+        return objectPool;
+    }
+
+    public int GetObjectCount()
+    {
+        return objectPool.Count;
+    }
+
     void Start()
     {
+
 
     }
 
