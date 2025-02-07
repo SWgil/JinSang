7c92e0cd61c055a82588a8d0e27b89ea2d2a2d77
SWgil <153619205+SWgil@users.noreply.github.com>
2025-02-07 22:51:00 +0900

    feat: reset 버튼 구현

--- JINSU/Domino/Assets/CodeScript/UI/InGameUIManager.cs
+++ JINSU/Domino/Assets/CodeScript/UI/InGameUIManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameUIManager : MonoBehaviour
 {
@@ -11,6 +12,6 @@ public class InGameUIManager : MonoBehaviour
 
     public void ResetRound()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
