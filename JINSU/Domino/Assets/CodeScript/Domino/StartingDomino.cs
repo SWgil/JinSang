56bbe1086f1b22066c9534963ab914e67bda35ec
SWgil <153619205+SWgil@users.noreply.github.com>
2025-03-03 15:35:34 +0900

    fix: Topviewmap에 StartingDomino를 추가, 회전 방향을 local축 기준으로 수정

--- JINSU/Domino/Assets/CodeScript/Domino/StartingDomino.cs
+++ JINSU/Domino/Assets/CodeScript/Domino/StartingDomino.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class StartingDomino : MonoBehaviour
 {
     private float torquePower = 1f;
-    private Vector3 torqueAxis = new Vector3(1, 0, 0);
+    private Vector3 torqueAxis = new Vector3(0, 0, -1);
 
     public void Activate()
     {
         Rigidbody rigidBody = GetComponent<Rigidbody>();
-        rigidBody.AddTorque(torqueAxis * torquePower, ForceMode.Impulse);
+        Vector3 localTorqueAxis = transform.TransformDirection(torqueAxis);
+        rigidBody.AddTorque(localTorqueAxis * torquePower, ForceMode.Impulse);
     }
 
     public void Reset()
